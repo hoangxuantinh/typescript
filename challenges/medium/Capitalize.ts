d2c846398841978cfe1e37c8761dc84ff09285ef
tinh hoang <tinh.hoang@enouvo.com>
2024-10-21 17:35:46 +0700

    add append args

--- challenges/medium/Capitalize.ts
+++ challenges/medium/Capitalize.ts
@@ -1,7 +1,17 @@
 // type capitalized = Capitalize<'hello world'> // expected to be 'Hello world'
-type MyCapitalized<T extends string> = T extends `${infer x}${infer tail}`
+type MyCapitalized1<T extends string> = T extends `${infer x}${infer tail}`
   ? `${Uppercase<x>}${tail}`
   : T;
 
+type TestMyCapitalized = MyCapitalized1<"hello word!">;
 
-type TestMyCapitalized = MyCapitalized<'hello word!'>
+
+// Note: type script sẽ infer những phần trước tail theo khoảng chia nhỏ nhất cho 1 string
+type MyCapitalized2<T extends string> = T extends `${infer x}${infer y}${infer tail}`
+  ? `${x}${Uppercase<y>}${tail}`
+  : T;
+
+
+
+type TestMyCapitalized2 = MyCapitalized2<"hello word!">; // "hEllo word!"
+ 
